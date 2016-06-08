3f5aaf3accfc0c72021af548921e25f60c358b71
= <sepand.haghighi@yahoo.com>
2016-06-09 02:41:19 +0430

    radio button added

--- advance_calc/calc.py
+++ advance_calc/calc.py
@@ -1,6 +1,5 @@
 from tkinter import *
 import math
-
 def frame_maker(root,side):
     temp=Frame(root)
     temp.pack(side=side,expand=YES,fill=BOTH)
@@ -16,24 +15,36 @@ def calc(w):
         w.set("Error")
 def tri_func(w,part="sin"):
     try:
+        if radio.get()==2:
+            temp= (float(w.get())*math.pi)/180
+        else:
+            temp=float(w.get())
         if part=="sin": 
-            w.set(math.sin(float(w.get())))
+            w.set(math.sin(temp))
         elif part=="cos":
-            w.set(math.cos(float(w.get())))
+            w.set(math.cos(temp))
         elif part=="tan":
-            w.set(math.tan(float(w.get())))
+            w.set(math.tan(temp))
         elif part=="fact":
-            w.set(math.factorial(int(w.get())))
+            w.set(math.factorial(temp))
     except:
         w.set("Error")
-        
+def radio_change(v):
+    if v==2:
+        label_rad.config(text="DEG")
+    else:
+        label_rad.config(text="RAD")
+
 object_list=["123","456","789"]
 if __name__=="__main__":
+    flag=0
     s=Frame(bg="gray40")
     s.pack(expand=YES,fill=BOTH)
     display=StringVar()
+    radio=IntVar()
     disp=Entry(s,textvariable=display)
     disp.pack(side=TOP,expand=YES,fill=BOTH)
+    frame_0=frame_maker(s,TOP)
     frame_1=frame_maker(s,TOP)
     frame_2=frame_maker(s,TOP)
     frame_3=frame_maker(s,TOP)
@@ -41,6 +52,13 @@ if __name__=="__main__":
     frame_5=frame_maker(s,TOP)
     frame_7=frame_maker(s,TOP)
     frame_6=frame_maker(s,TOP)
+    radio_1=Radiobutton(frame_0,variable=radio,text="RAD",value=1,command=lambda :radio_change(1))
+    radio_1.pack(anchor=W)
+    radio_1.select()
+    radio_2=Radiobutton(frame_0,variable=radio,text="DEG",value=2,command=lambda : radio_change(2))
+    radio_2.pack(anchor=W)
+    label_rad=Label(frame_0,text="RAD",fg="red")
+    label_rad.pack(anchor=W)
     button_1=button_maker(frame_1,"1",LEFT,lambda w=display:w.set(w.get()+"1"))
     button_2=button_maker(frame_1,"2",LEFT,lambda w=display:w.set(w.get()+"2"))
     button_3=button_maker(frame_1,"3",LEFT,lambda w=display:w.set(w.get()+"3"))
@@ -56,6 +74,7 @@ if __name__=="__main__":
     button_10=button_maker(frame_4,"+",LEFT,lambda w=display:w.set(w.get()+"+"))
     button_11=button_maker(frame_4,"-",LEFT,lambda w=display:w.set(w.get()+"-"))
     button_12=button_maker(frame_4,"/",LEFT,lambda w=display:w.set(w.get()+"/"))
+    button_pow=button_maker(frame_4,"^",LEFT,lambda w=display:w.set(w.get()+"**"))
     button_13=button_maker(frame_5,"*",LEFT,lambda w=display:w.set(w.get()+"*"))
     button_14=button_maker(frame_5,"0",LEFT,lambda w=display:w.set(w.get()+"0"))
     button_15=button_maker(frame_5,"=",LEFT,lambda w=display:calc(w))
