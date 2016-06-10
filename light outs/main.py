0a117a585a4c8a94392493c2a1391fc295db322e
= <sepand.haghighi@yahoo.com>
2016-06-11 03:47:58 +0430

    rand_gen added to light outs

--- light outs/main.py
+++ light outs/main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from random import *
+
 def but_func(x,y):
     if list(but_array[x][y].config()["background"])[-1]=="black":
         but_array[x][y].config(bg="white")
@@ -26,6 +28,15 @@ def but_func(x,y):
             but_array[x][y-1].config(bg="white")
         else:
             but_array[x][y-1].config(bg="black")
+def rand_gen():
+    rand_col=[]
+    rand_row=[]
+    for i in range(3):
+        rand_col.append(randint(0,4))
+        rand_row.append(randint(0,4))
+    but_array[rand_row[0]][rand_col[0]].config(bg="white")
+    but_array[rand_row[1]][rand_col[1]].config(bg="white")
+    but_array[rand_row[2]][rand_col[2]].config(bg="white")
 if __name__=="__main__":
     but_array=[]
     root=Frame()
@@ -41,6 +52,7 @@ if __name__=="__main__":
             but_temp.append(Button(frame,text="",command=lambda k=i, w=j:but_func(k,w),bg="black"))
             but_temp[j].pack(side=LEFT,expand=YES,fill=BOTH)
         but_array.append(but_temp)
+    rand_gen()
     root.mainloop()
         
             
